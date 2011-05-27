4e461f8ca63fa2ca3c365643ea58d3d7578074c6
Carl Jackson & Matthew Kocher <pair+carl+mkocher@pivotallabs.com>
2011-05-27 10:17:02 -0700

    DRYing up the enyo version string

--- lib/eris/lib/server.rb
+++ lib/eris/lib/server.rb
@@ -23,7 +23,7 @@ module Eris
 
         locals = {
             :app_title => app_info["title"],
-            :enyo_path => "enyo/0.10/framework/enyo.js",
+            :enyo_root => "",
             :eris_helper_tags => '<script src="eris-helpers/ErisHelpers.js" type="text/javascript" eris-helpers=true></script>',
             :app_helper_tag => nil,
             :launch_params => params['launchParams']
