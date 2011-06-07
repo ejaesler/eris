225192a2a391d9db90d393f7fa7130c6ec34a124
Davis Frank & Omer Spillinger <pair+davis+omer@pivotallabs.com>
2011-06-07 10:18:27 -0700

    Added --port/-p switch to run on a given port

--- lib/eris/tasks/server.rb
+++ lib/eris/tasks/server.rb
@@ -2,11 +2,13 @@ require 'thor'
 
 module Eris
   class Tasks < Thor
+    method_option :port, :default => "9999", :aliases => "-p", :desc => "which port you want server run on"
 
     desc "server", "Launch the Eris server for your Enyo app"
 
     def server
-      exec("rackup -p 9999 #{@@source_root}/lib/eris/eris.ru")
+      puts "Starting Eris on port #{options["port"]}"
+      exec("rackup -p #{options["port"]} #{@@source_root}/lib/eris/eris.ru")
     end
   end
 end
