e5293bc1967fe9bd62ebe7bca3c4c0be56d8f5b9
Davis W. Frank & Matthew Kocher <pair+dwfrank+mkocher@pivotallabs.com>
2011-06-08 12:09:44 -0700

    Better testing for optional port for server

--- lib/eris/tasks/server.rb
+++ lib/eris/tasks/server.rb
@@ -2,13 +2,13 @@ require 'thor'
 
 module Eris
   class Tasks < Thor
-    method_option :port, :default => "9999", :aliases => "-p", :desc => "which port you want server run on"
-
     desc "server", "Launch the Eris server for your Enyo app"
+    method_option :port, :default => "9999", :aliases => "-p", :desc => "server port"
 
     def server
-      puts "Starting Eris on port #{options["port"]}"
-      exec("rackup -p #{options["port"]} #{@@source_root}/lib/eris/eris.ru")
+      port = options[:port]
+      puts "Starting Eris on port #{port}"
+      exec("rackup -p #{port} #{@@source_root}/lib/eris/eris.ru")
     end
   end
 end
--- spec/lib/eris/tasks/server_spec.rb
+++ spec/lib/eris/tasks/server_spec.rb
@@ -1,19 +1,41 @@
 require 'spec_helper'
 
 describe Eris, "`server`" do
-  before :each do
-    @server_pid = fork do
-      @thor = Thor.new
-      sample_app_dir = File.join(File.dirname(__FILE__), '..', '..', '..', 'fixtures', 'sample_app/')
-      Dir.chdir sample_app_dir do
-        @thor.invoke Eris::Tasks, "server"
+
+  describe "(with defaults)" do
+    before :each do
+      @server_pid = fork do
+        sample_app_dir = File.join(File.dirname(__FILE__), '..', '..', '..', 'fixtures', 'sample_app/')
+        Dir.chdir sample_app_dir do
+          exec("eris server")
+        end
       end
+      sleep 2
     end
-    sleep 2
+
+    it "should start the Eris server on the default port" do
+      `curl -s http://localhost:9999/`.should match("<html>")
+    end
   end
 
-  it "should start the Eris server on the default port" do
-    `curl -s http://localhost:9999/`.should match("<html>")
+  describe "(with optional port)" do
+    let :port do
+      '6578'
+    end
+
+    before :each do
+      @server_pid = fork do
+        sample_app_dir = File.join(File.dirname(__FILE__), '..', '..', '..', 'fixtures', 'sample_app/')
+        Dir.chdir sample_app_dir do
+          exec("eris server --port=#{port}")
+        end
+      end
+      sleep 2
+    end
+
+    it "should start the Eris server on the requested port" do
+      `curl -s http://localhost:#{port}/`.should match("<html>")
+    end
   end
 
   after :each do
